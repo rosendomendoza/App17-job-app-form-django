7d70bbd87dd1f53187d39db3da424e2f8d3f74e0
ROSENDO MENDOZA <rosendo.arturo@gmail.com>
2023-11-07 11:26:02 +0100

    Day 54: Customize admin module

--- job_application/admin.py
+++ job_application/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Form
 
 
-admin.site.register(Form)
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "date", "occupation")
+    list_filter = ("date", "occupation")
+    readonly_fields = ("occupation",)
+    ordering = ("last_name", "date")
+    search_fields = ("last_name", "date")
+
+
+admin.site.register(Form, FormAdmin)
